9c8d8760410b6d5e97a55857d2f793cf019732a6
Charles Lucas Pedigo <charles_pedigo@mymail.eku.edu>
2026-01-27 11:24:28 -0500

    Initial Commit

---
+++ M2Calculator_Pedigo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M2Calculator_Pedigo
+//
+//  Created by Pedigo, Charles L. on 1/27/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
